7f39114d6c8f37058b6b09b6835ed9a81707c43f
Jairo Matías <jairo_1829@hotmail.com>
2026-08-17 09:13:08 -0400

    test: align resource package with final namespace

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -2,6 +2,7 @@ package com.example
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import com.jairomatias.digitalsanctuary.R
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
